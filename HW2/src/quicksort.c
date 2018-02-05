822d1779a010be1702748f26e86b8d969cf53995
jcelik <julius.cc@hotmail.com>
2018-02-05 18:05:07 +0100

    debug more

--- HW2/src/quicksort.c
+++ HW2/src/quicksort.c
@@ -15,10 +15,10 @@ double drand(double low, double high)
 double copy(double mainList[], double copyList[])
 {
 	int i;
-	// for (i = 0; i < sizeof(copyList) / sizeof(copyList[0]); i++)
-	// {
-	// 	copyList[i] = mainList[i];
-	// }
+	for (i = 0; i < sizeof(copyList) / sizeof(copyList[0]); i++)
+	{
+		copyList[i] = mainList[i];
+	}
 }
 
 /* Insertion sort is effective on smaller lists */
