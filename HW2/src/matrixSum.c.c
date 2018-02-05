b38d4f44636376377d463c60078a765debd03fdd
jcelik <julius.cc@hotmail.com>
2018-02-05 15:52:41 +0100

    Better table

--- HW2/src/matrixSum.c.c
+++ HW2/src/matrixSum.c.c
@@ -152,7 +152,7 @@ int main(int argc, char *argv[])
 	printf("Workers = [1, 2, 4, 8, 16, 24]\n");
 	printf("Sizes of matrix = [100, 1000, 10000]\n");
 	printf("Table:\n");
-	printf("|Workers\t|\t100\t|\t1000\t|\t10000\t|\n");
+	printf("|Workers:\\Size:\t|\t100\t|\t1000\t|\t10000\t|\n");
 
 	int i, j;
 	for(i = 0; i < sizeof(workers) / sizeof(workers[0]); i++)
