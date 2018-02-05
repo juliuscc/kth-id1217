bd249e165ad074d2a6c1398bf37ffa437a42d72d
jcelik <julius.cc@hotmail.com>
2018-02-05 14:50:08 +0100

    copy over first solution to b and c

--- HW2/src/matrixSum.c.c
+++ HW2/src/matrixSum.c.c
@@ -46,18 +46,55 @@ int main(int argc, char *argv[])
 		//	  printf(" ]\n");
 	}
 
+	int max_i = 0;
+	int max_j = 0;
+	int min_i = 0;
+	int min_j = 0;
+	int max_i_shared = 0;
+	int max_j_shared = 0;
+	int min_i_shared = 0;
+	int min_j_shared = 0;
+
 	start_time = omp_get_wtime();
-#pragma omp parallel for reduction(+ \
-								   : total) private(j)
-	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
+
+#pragma omp parallel shared(max_i_shared, max_j_shared, min_i_shared, min_j_shared) private(j) firstprivate(max_i, max_j, min_i, min_j)
+	{
+#pragma omp for reduction(+:total) nowait
+		for (i = 0; i < size; i++)
+			for (j = 0; j < size; j++)
+			{
+				total += matrix[i][j];
+				if (matrix[i][j] > matrix[max_i][max_j])
+				{
+					max_i = i;
+					max_j = j;
+				}
+				if (matrix[i][j] < matrix[min_i][min_j])
+				{
+					min_i = i;
+					min_j = j;
+				}
+			}
+#pragma omp critical
 		{
-			total += matrix[i][j];
+			if (matrix[max_i][max_j] > matrix[max_i_shared][max_j_shared])
+			{
+				max_i_shared = max_i;
+				max_j_shared = max_j;
+			}
+			if (matrix[min_i][min_j] > matrix[min_i_shared][min_j_shared])
+			{
+				min_i_shared = min_i;
+				min_j_shared = min_j;
+			}
 		}
+	}
 	// implicit barrier
 
 	end_time = omp_get_wtime();
 
 	printf("the total is %d\n", total);
+	printf("the maximum element is: %d on coordinates: (%d;%d)\n", matrix[max_i_shared][max_j_shared], max_i_shared, max_j_shared);
+	printf("the minimum element is: %d on coordinates: (%d;%d)\n", matrix[min_i_shared][min_j_shared], min_i_shared, min_j_shared);
 	printf("it took %g seconds\n", end_time - start_time);
 }
