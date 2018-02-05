f24d2fcf313defbe6bce95d0baa76afafddffad7
jcelik <julius.cc@hotmail.com>
2018-02-05 14:17:09 +0100

    fixed stufffffs

--- HW2/src/matrixSum.a.c
+++ HW2/src/matrixSum.a.c
@@ -50,13 +50,7 @@ int main(int argc, char *argv[])
 	int max = matrix[0][0];
 
 	start_time = omp_get_wtime();
-#pragma omp parallel for reduction(+         \
-								   : total), \
-	reduction(max                            \
-			  : max_val),                    \
-	reduction(min                            \
-			  : min_val),                    \
-	private(j)
+#pragma omp parallel for reduction(+:total), reduction(max:max_val), reduction(min:min_val), private(j)
 	for (i = 0; i < size; i++)
 		for (j = 0; j < size; j++)
 		{
