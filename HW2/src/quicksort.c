60f2ccadec5980b1a9a36c50860da3fe28fd2a96
jcelik <julius.cc@hotmail.com>
2018-02-07 16:38:42 +0100

    okay.........

--- HW2/src/quicksort.c
+++ HW2/src/quicksort.c
@@ -85,7 +85,7 @@ double oneIteration(float *originalArray, int length)
 		printf("Error!\n");
 		exit(1);
 	}
-	tempArray[0] = 0;
+	tempArray[0] = originalArray[0];
 	// for (i = 0; i < length; i++)
 	// 	tempArray[i] = originalArray[i];
 
